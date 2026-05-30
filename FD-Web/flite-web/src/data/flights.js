f132b99b6f63683f5545bff180a0b030a7b66fd2
Sidd <tahasidd33@gmail.com>
2026-05-30 21:08:54 +0530

    Added Flights data

---
+++ FD-Web/flite-web/src/data/flights.js
@@ -0,0 +1,24 @@
+const flights = [
+    {
+        code: "EK 502",
+        from: "DXB",
+        to: "LHR",
+    },
+    {
+        code: "AI 203",
+        from: "DEL",
+        to: "BOM",
+    },
+    {
+        code: "QR 477",
+        from: "DOH",
+        to: "SIN",
+    },
+    {
+        code: "SQ 245",
+        from: "SIN",
+        to: "JFK",
+    },
+];
+
+export default flights;
